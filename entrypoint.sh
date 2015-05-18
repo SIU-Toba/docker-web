1d4b2e2816249d43d51113c6e710419961d2ebd0
Andrés Blanco <andresblanco@gmail.com>
2015-05-18 11:22:57 -0300

    Fix de permisos en OSX

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Fix de permisos en OSX. Para que funcione hay que definir la variable de entorno OSX
+if [ ! -z "$OSX" ] && [ -z "`grep docker /etc/apache2/apache2.conf`" ]; then
+	# Using under boot2docker, fix rights
+	echo "Using 'docker/staff' for Apache2 in boot2docker . . ."
+
+	# Add a user for Apache2
+	useradd -u 1000 -g 50 docker
+
+	# Use the uid 1000 & gid 50 for Apache2
+	sed -i 's/User www-data/User docker/' /etc/apache2/apache2.conf
+	sed -i 's/Group www-data/Group staff/' /etc/apache2/apache2.conf
+fi
+
 for entrypoint in /entrypoint.d/*.sh
 do
     if [ -f $entrypoint -a -x $entrypoint ]
